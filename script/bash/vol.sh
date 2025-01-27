da5ee80f83635f60ff91ff89f873d412b1d16b68
thanakorn boonyasena <thanakorn.boonyasena@gmail.com>
2025-01-28 01:43:37 +0700

    mute

--- script/bash/vol.sh
+++ script/bash/vol.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 get_vol() {
 	status="$(pactl -f json list sinks)"
@@ -17,10 +17,21 @@ dec() {
 	pactl set-sink-volume @DEFAULT_SINK@ -8%
 }
 
+mute() {
+	pactl set-sink-mute @DEFAULT_SINK@ toggle
+	if [[ $(pactl get-sink-mute @DEFAULT_SINK@ | grep -oP '(?<=mute: )|yes|no') == "yes" ]]; then
+		eww update vol_dot_reveal=false
+	else
+		eww update vol_dot_reveal=true
+	fi
+}
+
 if [[ -z "$1" ]]; then
 	get_vol
 elif [[ "$1" == "--down" ]]; then
 	dec
 elif [[ "$1" == "--up" ]]; then
 	inc
+elif [[ "$1" == "--mute" ]]; then
+	mute
 fi
